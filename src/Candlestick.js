3903f7f365894bbcc725598f1f483f6dea48a3bf
Einar Huseby <einar.huseby@gmail.com>
2019-12-18 20:57:53 +0100

    Fixes #2

--- src/Candlestick.js
+++ src/Candlestick.js
@@ -1,13 +1,13 @@
 class Candlestick {
   constructor(open, product, timeframe, timestamp) {
-    this.timestamp = timestamp;
+    this.timestamp = +timestamp;
     this.timeframe = timeframe;
     this.product = product;
-    this.price = this.open;
+    this.price = +open;
     this.open = open;
-    this.high = this.open;
-    this.low = this.open;
-    this.close = this.open;
+    this.high = +open;
+    this.low = +open;
+    this.close = +open;
     this.volume = 0;
     this.size = 0;
     this.wick = {};
@@ -32,24 +32,37 @@ class Candlestick {
   }
 
   updatePrice(price, size) {
-    this.price = !this.closed ? Number.parseFloat(price).toFixed(8) : Number.parseFloat(this.price).toFixed(8);
-    this.high = this.price > this.high ? Number.parseFloat(this.price).toFixed(8) : Number.parseFloat(this.high).toFixed(8);
-    this.low = this.price < this.low ? Number.parseFloat(this.price).toFixed(8) : Number.parseFloat(this.low).toFixed(8);
-    this.close = Number.parseFloat(this.price).toFixed(8);
-    this.volume = (Number.parseFloat(this.volume) + Number.parseFloat(size)).toFixed(8);
-    this.size = (Number.parseFloat(this.high) - Number.parseFloat(this.low)).toFixed(8);
-    if (this.price >= this.open) {
-      this.wick.top = (Number.parseFloat(this.high) - Number.parseFloat(this.close)).toFixed(8);
-      this.wick.bottom = (Number.parseFloat(this.open) - Number.parseFloat(this.low)).toFixed(8);
-    } else {
-      this.wick.top = (Number.parseFloat(this.high) - Number.parseFloat(this.open)).toFixed(8);
-      this.wick.bottom = (Number.parseFloat(this.close) - Number.parseFloat(this.low)).toFixed(8);
+
+
+    this.price = !this.closed ? Number.parseFloat(price) : Number.parseFloat(this.price);
+
+    if (!this.open || this.open === 0) {
+      this.open = Number.parseFloat(this.price);
     }
-    this.wick.size = Math.abs(Number.parseFloat(this.wick.top) - Number.parseFloat(this.wick.bottom)).toFixed(8);
-    if (Number.parseFloat(this.size)> 0) {
-      this.wick.ratio = (Number.parseFloat(this.wick.size) / Number.parseFloat(this.size)).toFixed(8);
+
+    if (+this.price > +this.high || +this.high === 0) {
+      this.high = Number.parseFloat(this.price)
+    };
+    if (+this.price < +this.low || +this.low === 0) {
+      this.low = Number.parseFloat(this.price)
+    };
+
+    this.close = Number.parseFloat(this.price);
+
+    this.volume = (Number.parseFloat(this.volume) + Number.parseFloat(size));
+    this.size = (Number.parseFloat(this.high) - Number.parseFloat(this.low));
+    if (+this.price >= +this.open) {
+      this.wick.top = (Number.parseFloat(this.high) - Number.parseFloat(this.close));
+      this.wick.bottom = (Number.parseFloat(this.open) - Number.parseFloat(this.low));
     } else {
-      this.wick.ratio = Number.parseFloat(0).toFixed(8);
+      this.wick.top = (Number.parseFloat(this.high) - Number.parseFloat(this.open));
+      this.wick.bottom = (Number.parseFloat(this.close) - Number.parseFloat(this.low));
+    }
+    this.wick.size = Math.abs(Number.parseFloat(this.wick.top) - Number.parseFloat(this.wick.bottom));
+    if (Number.parseFloat(this.size) > 0) {
+      this.wick.ratio = (Number.parseFloat(this.wick.size) / Number.parseFloat(this.size));
+    } else {
+      this.wick.ratio = Number.parseFloat(0);
     }
     this.setMarket();
   }
